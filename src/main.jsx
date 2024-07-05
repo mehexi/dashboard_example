b04152141614aa22d4923d3bf64a9a4dc637c29b
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2024-07-05 23:45:36 +0600

    "Removed imports for createBrowserRouter and path from main.jsx, and removed two empty lines."

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider} from "react-router-dom";
 import AuthProvider from "./auth/AuthProvider";
 import router from "./routers/router";
-import path from "path";
-
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
