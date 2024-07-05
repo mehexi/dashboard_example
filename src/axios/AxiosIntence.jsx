5decdcdd1ae56b6ba423723ca78673f849e66d7a
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2024-07-05 11:23:50 +0600

    "Removed empty line and updated BASE_URL from hardcoded 'http://localhost:5001' to import.meta.env.VITE_URL"

--- src/axios/AxiosIntence.jsx
+++ src/axios/AxiosIntence.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = import.meta.env.VITE_URL;
 
 // Public axios instance
 const axiosInstance = axios.create({
